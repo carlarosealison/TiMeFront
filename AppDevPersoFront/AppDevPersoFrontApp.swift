0951c8da4a4e96129277ac258fa65729b7742b86
alicyarose@gmail.com <alicyarose@gmail.com>
2025-09-12 16:31:45 +0200

    Premier commit

---
+++ AppDevPersoFront/AppDevPersoFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevPersoFrontApp.swift
+//  AppDevPersoFront
+//
+//  Created by Apprenant125 on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppDevPersoFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDevPersoFront/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppDevPersoFront
+//
+//  Created by Apprenant125 on 12/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
