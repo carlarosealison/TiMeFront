6f2d11631df5bc037495a49fbbfb49775ecd2ae8
alicyarose@gmail.com <alicyarose@gmail.com>
2025-10-01 16:56:36 +0200

    Mise en place de la gravité sur la Ball dans JarView

---
+++ TiMeFront/Features/Jar/JarView/BallsView.swift
@@ -0,0 +1,28 @@
+//
+//  BallsView.swift
+//  TiMeFront
+//
+//  Created by Apprenant125 on 01/10/2025.
+//
+
+import SwiftUI
+import SpriteKit
+
+//struct BallsView: View {
+//    var scene : SKScene {
+//        let scene = BallsViewContainer()
+//        scene.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+//        scene.backgroundColor = .whitePurple
+//        
+//        return scene
+//    }
+//    
+//    var body: some View {
+//        SpriteView(scene: scene)
+//            .ignoresSafeArea()
+//    }
+//}
+//
+//#Preview {
+//    BallsView()
+//}
