40046ac1db9b1947a10c231761e6bc82c38e513d
Sébastien <sebbody79400@gmail.com>
2025-10-02 10:22:10 +0200

    Create component

---
+++ TiMeFront/Shared/Components/Indicators.swift
@@ -0,0 +1,18 @@
+//
+//  Indicators.swift
+//  TiMeFront
+//
+//  Created by Sebastien Besse on 02/10/2025.
+//
+
+import SwiftUI
+
+struct Indicators: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Indicators()
+}
