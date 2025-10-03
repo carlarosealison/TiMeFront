529e73eca8fdbcf5f9f50dc8883ecc78524f6000
Mounirbl <mounir.bouallel@outlook.fr>
2025-10-03 16:57:10 +0200

    Ajout du liquid glass sur les parametres

---
+++ TiMeFront/Features/Profil/ProfilModel/ProfilModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TiMeFront
+//
+//  Created by Mounir on 26/09/2025.
+//
+
+import Foundation
---
+++ TiMeFront/Features/Profil/ProfilViewModel/ProfilViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfilViewModel.swift
+//  TiMeFront
+//
+//  Created by Mounir on 26/09/2025.
+//
+
+import Foundation
