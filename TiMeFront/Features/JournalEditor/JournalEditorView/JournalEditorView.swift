d6ac68d94e42c3e92afeceac1b75357573376aca
alicyarose@gmail.com <alicyarose@gmail.com>
2025-10-22 19:41:33 +0200

    création des fichiers pour la branche JournalEditor

---
+++ TiMeFront/Features/JournalEditor/JournalEditorView/JournalEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalEditorView.swift
+//  TiMeFront
+//
+//  Created by Carla on 22/10/2025.
+//
+
+import SwiftUI
+
+struct JournalEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalEditorView()
+}
---
+++ TiMeFront/Features/JournalEditor/JournalEditorViewModel/JournalEditorViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  JournalEditorViewModel.swift
+//  TiMeFront
+//
+//  Created by Carla on 22/10/2025.
+//
+
+import Foundation
