577fc73a90f9b9404524743511496c454e1797d5
alicyarose@gmail.com <alicyarose@gmail.com>
2025-10-24 14:03:22 +0200

    changements mineurs + création du ScrollMotivation pour le JournalEditor

---
+++ TiMeFront/Shared/Components/ScrollMotivation.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollMotivation.swift
+//  TiMeFront
+//
+//  Created by Apprenant125 on 23/10/2025.
+//
+
+import SwiftUI
+
+struct ScrollMotivation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollMotivation()
+}
