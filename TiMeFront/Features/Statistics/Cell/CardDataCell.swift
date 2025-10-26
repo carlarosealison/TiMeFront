7c8ab8ee24a0e1aed4586e9ca02b7ff03cebbc1f
Sébastien <sebbody79400@gmail.com>
2025-10-26 23:50:33 +0100

    update card

--- TiMeFront/Features/Statistics/Cell/CardDataCell.swift
+++ TiMeFront/Features/Statistics/Cell/CardDataCell.swift
@@ -12,37 +12,45 @@ struct CardDataCell: View {
     let (width, height) : (CGFloat, CGFloat)
     let (nbStat, sizeS) : (Int, CGFloat)
     let (description, sizeD) : (String, CGFloat)
+    
     var isLargeCard: Bool
+    
+    let (smallNbStat, sizeSS) : (Int, CGFloat)
+    let (smallDescription, sizeSD) : (String, CGFloat)
+    
     var body: some View {
         if #available(iOS 26.0, *) {
-            ZStack{
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.purpleHover.opacity(0.8))
-                    .frame(width: width, height: height)
-                VStack{
-                    contentCard(nbStat: nbStat, description: description)
-                
-                    if isLargeCard{
-                        HStack(spacing: 100){
-                            contentCard(nbStat: nbStat, description: description)
-                            contentCard(nbStat: nbStat, description: description)
+            
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.purpleHover.opacity(0.8))
+                .frame(width: width, height: height)
+                .overlay{
+                    
+                        contentCard(nbStat: nbStat, description: description, sizeS: sizeS, sizeD: sizeD)
+                        if isLargeCard{
+                            HStack(spacing: 180){
+                                contentCard(nbStat: smallNbStat, description: smallDescription, sizeS: sizeSS, sizeD: sizeSD)
+                                
+                                contentCard(nbStat: smallNbStat, description: smallDescription, sizeS: sizeSS, sizeD: sizeSD)
+                            }
+                            .frame(height: 120, alignment: .bottom)
                         }
-                    }
+                    
                 }
                 .foregroundStyle(.white)
                 .bold()
-            }
-            .glassEffect(
-                in: RoundedRectangle(cornerRadius: 12)
-            )
-            .ignoresSafeArea()
+            
+                .glassEffect(
+                    in: RoundedRectangle(cornerRadius: 12)
+                )
+                .ignoresSafeArea()
         } else {
             // Fallback on earlier versions
         }
     }
     
-    func contentCard(nbStat: Int, description: String) -> some View{
-        VStack(alignment: .center, spacing: 10){
+    func contentCard(nbStat: Int, description: String, sizeS: CGFloat, sizeD: CGFloat) -> some View{
+        VStack(alignment: .center, spacing: 4){
             Text(String(nbStat))
                 .font(.system(size: sizeS).width(.expanded))
             Text(description)
@@ -54,6 +62,6 @@ struct CardDataCell: View {
 }
 
 #Preview {
-//    CardDataCell(size: CGSize(width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height/8),nbStat: 54, sizeS: 26, description: "Stricks max", sizeD: 8)
-    CardDataCell(width: 295, height: 143,nbStat: 54, sizeS: 26, description: "Stricks max", sizeD: 8, isLargeCard: false)
+    //    CardDataCell(size: CGSize(width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height/8),nbStat: 54, sizeS: 26, description: "Stricks max", sizeD: 8)
+    CardDataCell(width: 295, height: 143,nbStat: 54, sizeS: 26, description: "Stricks max", sizeD: 8, isLargeCard: true, smallNbStat: 12, sizeSS: 12, smallDescription: "Ce mois", sizeSD: 8)
 }
--- TiMeFront/Features/Statistics/Cell/GridCardDataCell.swift
+++ TiMeFront/Features/Statistics/Cell/GridCardDataCell.swift
@@ -12,21 +12,21 @@ struct GridCardDataCell: View {
     var challengeSuccessful: Int
     var body: some View {
         
-        VStack{
-            HStack {
-                CardDataCell(width: 141, height: 75, nbStat: 61, sizeS: 26, description: "Motivation moyenne", sizeD: 8, isLargeCard: false)
-                
-                CardDataCell(width: 217, height: 75, nbStat: challengeSuccessful, sizeS: 26, description: "Défi réussi", sizeD: 8, isLargeCard: false)
-            }
-            HStack {
-                    CardDataCell(width: 295, height: 143, nbStat: 55, sizeS: 26, description: "rédaction", sizeD: 8, isLargeCard: true)
-                    VStack{
-                        CardDataCell(width: 65, height: 65, nbStat: streak, sizeS: 26, description: "Streak", sizeD: 8, isLargeCard: false)
-                        
-                        CardDataCell(width: 65, height: 65, nbStat: 4, sizeS: 26, description: "Livres", sizeD: 8, isLargeCard: false)
-                    }
-            }
-        }
+//        VStack{
+//            HStack {
+//                CardDataCell(width: 141, height: 75, nbStat: 61, sizeS: 26, description: "Motivation moyenne", sizeD: 8, isLargeCard: false)
+//                
+//                CardDataCell(width: 217, height: 75, nbStat: challengeSuccessful, sizeS: 26, description: "Défi réussi", sizeD: 8, isLargeCard: false)
+//            }
+//            HStack {
+//                    CardDataCell(width: 295, height: 143, nbStat: 55, sizeS: 26, description: "rédaction", sizeD: 8, isLargeCard: true)
+//                    VStack{
+//                        CardDataCell(width: 65, height: 65, nbStat: streak, sizeS: 26, description: "Streak", sizeD: 8, isLargeCard: false)
+//                        
+//                        CardDataCell(width: 65, height: 65, nbStat: 4, sizeS: 26, description: "Livres", sizeD: 8, isLargeCard: false)
+//                    }
+//            }
+//        }
     }
 }
 
